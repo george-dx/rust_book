fb4d17dd326a0f9df2bdb86d5b843b1bab512bb1
George Daian <georgedaianx@gmail.com>
2023-06-25 17:02:02 +0300

    Defining and Instantiating Structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+// We want each instance of this struct to own all of its data
+// and for that data to be valid for as long as the entire struct
+// is valid
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+struct AlwaysEqual;
+
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    println!("User 2 email: {0}", user2.email);
+
+    let _black = Color(0, 0, 0);
+    let _origin = Point(0, 0, 0);
+
+    let _subject = AlwaysEqual;
 }
