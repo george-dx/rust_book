02aa815fa80874ff5e0da9cae24cf4888e9e96fc
George Daian <georgedaianx@gmail.com>
2023-07-03 21:32:10 +0300

    Add UTF-8 String examples from simple to more advanced

--- chapter8/src/main.rs
+++ chapter8/src/main.rs
@@ -5,6 +5,7 @@ enum SpreadshetCell {
 }
 
 fn main() {
+    // Vectors
     let _v1: Vec<i32> = Vec::new();
     let _v2 = vec![1, 2, 3];
 
@@ -48,6 +49,63 @@ fn main() {
         SpreadshetCell::Int(3),
         SpreadshetCell::Text(String::from("blue")),
         SpreadshetCell::Float(10.12),
-    ]
+    ];
 
+    // Strings
+    let mut s = String::new();
+    let data = "initital content";
+    let ss = data.to_string();
+    let sss = "initial cont".to_string();
+    let ssss = String::from("initial contents");
+
+    let hello = String::from("عليكم السلام");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("םֹלוָׁש;");
+    let hello = String::from("");
+    let hello = String::from("こんにちは");
+    let hello = String::from("");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("{}", s);
+
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("{}", s);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from(" World!");
+    
+    // fn add(self, s: &str) -> String { -> this is how it is
+    // implemented. The reason we’re able to use &s2 in the 
+    // call to add is that the compiler can coerce the &String 
+    // argument into a &str. 
+    let s3 = s1 + &s2;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    let s1 = String::from("tic");
+    // we can use instead the format! macro for combining
+    // string in more complicated ways
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("{}", s);
+
+    // The best way to operate on pieces of strings is to 
+    // be explicit about whether you want characters or 
+    // bytes.
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
 }
