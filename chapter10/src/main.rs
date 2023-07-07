f046be693cf825dcc0b3a163ce766d39eac90f02
George Daian <georgedaianx@gmail.com>
2023-07-07 14:03:48 +0300

    Add generics in method definitions

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -45,6 +45,34 @@ struct ComplexPoint<T, U> {
     y: U,
 }
 
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct MixedPoint<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> MixedPoint<X1, Y1> {
+    fn mixup<X2, Y2>(
+        self,
+        other: MixedPoint<X2, Y2>,
+    ) -> MixedPoint<X1, Y2> {
+        MixedPoint {
+            x: self. x,
+            y: other.y,
+        }
+    }
+}
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
@@ -62,4 +90,14 @@ fn main() {
     // let wont_work = Point {x: 5, y: 4.0};
 
     let integer_float_point = ComplexPoint {x: 5, y:4.0 };
+
+    let p = Point {x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+
+    let p1 = MixedPoint {x: 5, y: 10.4};
+    let p2 = MixedPoint {x: "Hello", y: 'c'};
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
