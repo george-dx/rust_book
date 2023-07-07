61aafad07a78f3526df31c461374d470c729450c
George Daian <georgedaianx@gmail.com>
2023-07-07 13:16:26 +0300

    Transform largest function in a generic function

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
     let mut largest = &list[0];
 
     for item in list {
@@ -6,18 +6,42 @@ fn largest(list: &[i32]) -> &i32 {
             largest = item;
         }
     }
-    
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
     largest
 }
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
     let result = largest(&number_list);
     println!("The largest number is {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {result}");
 }
