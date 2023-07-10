4fe7be9e6ad3759ee71b9b2d234a5b73df834e9c
George Daian <georgedaianx@gmail.com>
2023-07-10 16:58:55 +0300

    Add traits as parameters

--- chapter10/src/lib.rs
+++ chapter10/src/lib.rs
@@ -2,11 +2,12 @@ pub trait Summary {
     fn summarize_author(&self) -> String;
 
     fn summarize(&self) -> String {
-        format!(
-            "(Read more from {}...)",
-            self.summarize_author()
-        )
+        format!("(Read more from {}...)", self.summarize_author())
     }
+}
+
+pub trait Display {
+    fn display(&self) -> String;
 }
 
 pub struct NewsArticle {
@@ -33,4 +34,20 @@ impl Summary for Tweet {
     fn summarize_author(&self) -> String {
         format!("@{}", self.username)
     }
+}
+
+impl Display for Tweet {
+    fn display(&self) -> String {
+        format!("ABCD")
+    }
+}
+
+impl Display for NewsArticle {
+    fn display(&self) -> String {
+        format!("ABCD")
+    }
+}
+
+pub fn notify(item: &(impl Summary + Display)) {
+    println!("Breaking news! {}", item.summarize());
 }
