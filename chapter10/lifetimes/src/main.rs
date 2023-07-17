03321e04c55bedf226a0268d25de1ea2158a0d5c
George Daian <georgedaianx@gmail.com>
2023-07-17 20:04:20 +0300

    Add lifetime ellision

--- chapter10/lifetimes/src/main.rs
+++ chapter10/lifetimes/src/main.rs
@@ -1,9 +1,25 @@
-fn longest<'a>(x: &'a str, y:&'a str) -> &'a str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
         y
     }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
 
 fn main() {
@@ -28,4 +44,12 @@ fn main() {
     }
     // println!("The longest string is {result}"); // won't compile - borrowed
     // value does not live long enough
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
