1437392219f89c4b1be277121f3b469b77be4834
George Daian <georgedaianx@gmail.com>
2023-07-17 20:11:27 +0300

    Add static lifetime; generic type parameters, trait bounds and lifetimes togheter

--- chapter10/lifetimes/src/main.rs
+++ chapter10/lifetimes/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -20,6 +22,22 @@ fn first_word(s: &str) -> &str {
     }
 
     &s[..]
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
 
 fn main() {
