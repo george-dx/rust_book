2df75adc33ec95455d7148d646f42dbfe9634d1b
George Daian <georgedaianx@gmail.com>
2023-07-22 16:28:36 +0300

    Test equality with assert_eq! and assert_ne! macros

--- chapter11/adder/src/lib.rs
+++ chapter11/adder/src/lib.rs
@@ -14,6 +14,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,5 +59,15 @@ mod tests {
         };
 
         assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn it_adds_two_negative_test() {
+        assert_ne!(5, add_two(2));
     }
 }
