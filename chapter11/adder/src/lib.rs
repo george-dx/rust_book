57f05b05e25769acfd60f13cca55c1a86a1a5cbe
George Daian <georgedaianx@gmail.com>
2023-07-25 14:59:58 +0300

    Write test using Result<T,E>

--- chapter11/adder/src/lib.rs
+++ chapter11/adder/src/lib.rs
@@ -99,4 +99,13 @@ mod tests {
             "Greeting did not contain name, value was `{result}`"
         );
     }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 }
