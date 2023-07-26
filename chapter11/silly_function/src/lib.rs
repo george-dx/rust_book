44fca56e4534428a26d79977fb6c1cc5e145b739
George Daian <georgedaianx@gmail.com>
2023-07-26 17:43:14 +0300

    Add multiple test in order to run a subset of tests by name

--- chapter11/silly_function/src/lib.rs
+++ chapter11/silly_function/src/lib.rs
@@ -3,6 +3,10 @@ fn prints_and_returns_10(a: i32) -> i32 {
     10
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -17,5 +21,20 @@ mod tests {
     fn this_test_will_fail() {
         let value = prints_and_returns_10(8);
         assert_eq!(5, value);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
     }
 }
