7ad432847d6f1060345152649f0f26587d45714d
George Daian <georgedaianx@gmail.com>
2023-08-02 12:49:03 +0300

    Read from file; add test file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    // The program's name takes up the first value in the
-    // vector at args[0], so we're starting arguments at
-    // index 1
-    let query = &args[1];
-    let file_path = &args[2];
+
+    // The program's name takes up the first value in the vector at args[0], so we're starting arguments at index 1
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents: String = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
