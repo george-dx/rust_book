d7e0fc9d48a7ba6906acbdd86a1050fd897623a8
George Daian <georgedaianx@gmail.com>
2023-08-02 13:16:09 +0300

    Refactor to improve - separate concerns for binary projects

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,30 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // The program's name takes up the first value in the vector at args[0], so we're starting arguments at index 1
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+
+        Config {query, file_path}
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // The program's name takes up the first value in the vector at args[0], so we're starting arguments at index 1
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+    let config: Config = Config::new(&args);
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let contents: String = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents: String = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
