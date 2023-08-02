b6b495595a0b34a35d12758e8f31f51333e452c3
George Daian <georgedaianx@gmail.com>
2023-08-02 15:33:03 +0300

    Refactor to improve modularity - extract logic from main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 struct Config {
     query: String,
@@ -16,23 +17,32 @@ impl Config {
         let query: String = args[1].clone();
         let file_path: String = args[2].clone();
 
-        Ok(Config { query, file_path })
+        Ok(Config {query, file_path})
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config: Config = Config::build(&args).unwrap_or_else(|err|
+    {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
+    
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents: String =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
