af8b899dcafe2230533b4c6ec5a9cec6ca29e401
George Daian <georgedaianx@gmail.com>
2023-08-02 12:36:13 +0300

    Accept command line arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    // The program's name takes up the first value in the
+    // vector at args[0], so we're starting arguments at
+    // index 1
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
