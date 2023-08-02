d0cbf9d828e846b92ae2409d2eae6e0bb59bb17f
George Daian <georgedaianx@gmail.com>
2023-08-02 14:22:10 +0300

    Refactor to improve - fix the error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,24 +8,31 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
         // The program's name takes up the first value in the vector at args[0], so we're starting arguments at index 1
         let query: String = args[1].clone();
         let file_path: String = args[2].clone();
 
-        Config {query, file_path}
+        Ok(Config { query, file_path })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::new(&args);
-    
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents: String = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    let contents: String =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
