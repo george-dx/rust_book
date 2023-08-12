56724463da33dd4e6bc5aed8c12ba8260ff4f6f3
George Daian <georgedaianx@gmail.com>
2023-08-12 15:25:46 +0300

    Capture mutable reference

--- chapter13/src/main.rs
+++ chapter13/src/main.rs
@@ -60,12 +60,14 @@ fn main() {
 
     println!("{}", expensive_closure(32));
 
-    let list = vec![1, 2, 3];
+    let mut list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
 
-    let only_borrows = || println!("From closure: {:?}", list);
+    let mut borrows_mutably = || list.push(7);
+    // let only_borrows = || println!("From closure: {:?}", list);
 
-    println!("Before calling closure: {:?}", list);
-    only_borrows();
+    // println!("Before calling closure: {:?}", list);
+    // only_borrows();
+    borrows_mutably();
     println!("After calling closure: {:?}", list);
 }
