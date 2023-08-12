135f755d0c2cee317ff306b5a9368d1bcad2e4d6
George Daian <georgedaianx@gmail.com>
2023-08-12 16:23:12 +0300

    Move captured values out of closures | Fn Traits

--- chapter13/src/main.rs
+++ chapter13/src/main.rs
@@ -6,6 +6,12 @@ enum ShirtColor {
     Blue,
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 struct Inventory {
     shirts: Vec<ShirtColor>,
 }
@@ -70,4 +76,22 @@ fn main() {
     // only_borrows();
     borrows_mutably();
     println!("After calling closure: {:?}", list);
+
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
 }
