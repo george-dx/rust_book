5168ad6c1b30ab1bf3d6c339971e197a4e65cb5e
George Daian <georgedaianx@gmail.com>
2023-08-25 18:12:56 +0300

    Add test for method that produce other iterators

--- chapter13/src/lib.rs
+++ chapter13/src/lib.rs
@@ -17,3 +17,11 @@ fn iterator_sum() {
 
     assert_eq!(total, 6);
 }
+
+#[test]
+fn iterator_map() {
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
+}
