bae088b2f28997a739d6358388e91a5b1ab539ea
George Daian <georgedaianx@gmail.com>
2023-08-25 15:12:50 +0300

    Add intro to processing a series of items with iterators

--- chapter13/src/main.rs
+++ chapter13/src/main.rs
@@ -94,4 +94,11 @@ fn main() {
 
     list.sort_by_key(|r| r.width);
     println!("{:#?}", list);
+
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {val}");
+    }
 }
