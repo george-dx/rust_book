8db6af9d9673ffa284701afb1fa07fc686a1f5f8
George Daian <georgedaianx@gmail.com>
2023-09-13 20:42:48 +0300

    Enable recursive types with boxes

--- chapter15/src/main.rs
+++ chapter15/src/main.rs
@@ -1,5 +1,14 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
     // store an i32 value on the heap
     let b = Box::new(5);
     println!("b = {b}");
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
