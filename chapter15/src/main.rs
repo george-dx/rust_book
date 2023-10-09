f4e4edc1e5ba3fd7980d1919f3a17ef97d4d4109
George Daian <georgedaianx@gmail.com>
2023-10-09 21:00:26 +0300

    Add counters for strong and weak references

--- chapter15/src/main.rs
+++ chapter15/src/main.rs
@@ -157,16 +157,22 @@ fn main() {
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf strong = {}, weak={}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!("branch strong = {}, weak={}", Rc::strong_count(&branch), Rc::weak_count(&branch));
+
+        println!("leaf strong = {}, weak={}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+    }
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-
-    let branch = Rc::new(Node {
-        value: 5,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
-
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!("leaf strong = {}, weak={}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
 }
 // 18793
