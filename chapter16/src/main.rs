b747fa9e5c3cfb44f8e76dde290fd499f25cbe62
George Daian <georgedaianx@gmail.com>
2023-10-14 12:55:06 +0300

    Create multpile producers

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -29,6 +29,7 @@ fn main() {
     }
 
     let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
 
     thread::spawn(move || {
         let vals = vec![
@@ -36,6 +37,19 @@ fn main() {
             String::from("from"),
             String::from("the"),
             String::from("thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
         ];
         for val in vals {
             tx.send(val).unwrap();
