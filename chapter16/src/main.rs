eb34ca62d938dcfa398f1c01648d8a6d8db43041
George Daian <georgedaianx@gmail.com>
2023-10-14 12:49:20 +0300

    Use message passing to transfer data between threads

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -1,26 +1,40 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
+    let first_part = false;
+    if first_part {
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {i} from the spawned thread!");
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {i} from the main thread!");
             thread::sleep(Duration::from_millis(1));
         }
-    });
 
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
+        handle.join().unwrap();
+
+        let v = vec![1, 2, 3];
+
+        let another_handle = thread::spawn(move || {
+            println!("Here's a vector: {:?}", v);
+        });
+
+        another_handle.join().unwrap();
     }
 
-    handle.join().unwrap();
+    let (tx, rx) = mpsc::channel();
 
-    let v = vec![1, 2, 3];
-
-    let another_handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
     });
 
-    another_handle.join().unwrap();
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
 }
