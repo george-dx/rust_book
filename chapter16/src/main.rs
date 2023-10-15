3ec66e8630f2014d4e3dc0c321356d8946a75525
George Daian <georgedaianx@gmail.com>
2023-10-15 16:09:30 +0300

    Play with the API of mutex

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -1,6 +1,6 @@
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc;
+use std::sync::{mpsc, Mutex};
 
 fn main() {
     let first_part = false;
@@ -28,36 +28,48 @@ fn main() {
         another_handle.join().unwrap();
     }
 
-    let (tx, rx) = mpsc::channel();
-    let tx1 = tx.clone();
+    let second_part = false;
+    if second_part {
+        let (tx, rx) = mpsc::channel();
+        let tx1 = tx.clone();
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("thread"),
+            ];
+            for val in vals {
+                tx1.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        for received in rx {
+            println!("Got: {received}");
         }
-    });
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {received}");
     }
+
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    
+    println!("m = {:?}", m);
 }
