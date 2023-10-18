40155342f42c4354c53502f67afc1678c3ca9b22
George Daian <georgedaianx@gmail.com>
2023-10-18 20:36:34 +0300

    Change Rc to Arc to be able to access data from one thread at a time

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -1,6 +1,6 @@
 use std::thread;
 use std::time::Duration;
-use std::sync::{mpsc, Mutex};
+use std::sync::{mpsc, Mutex, Arc};
 use std::rc::Rc;
 
 fn main() {
@@ -65,11 +65,11 @@ fn main() {
         }
     }
 
-    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
