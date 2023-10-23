d18104b8d190b2af5724dd2051797cb3e9f78d5d
George Daian <georgedaianx@gmail.com>
2023-10-23 15:53:38 +0300

    Add chapter17 workspace, update chapter16 to contain 3 different sections

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -65,21 +65,24 @@ fn main() {
         }
     }
 
-    let counter = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
+    let third_part = false;
+    if third_part { 
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
 
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
-            *num += 1;
-        });
-        handles.push(handle);
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result: {}", *counter.lock().unwrap());
     }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!("Result: {}", *counter.lock().unwrap());
 }
