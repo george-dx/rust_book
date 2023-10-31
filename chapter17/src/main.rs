39a0e9432022eb9b78123ca5d064fab0248a4565
George Daian <georgedaianx@gmail.com>
2023-10-31 16:06:27 +0200

    Refactor code to take advantage of Rust

--- chapter17/src/main.rs
+++ chapter17/src/main.rs
@@ -71,14 +71,24 @@ fn main() {
         screen.run();
     }
 
+    // let mut post = Post::new();
+
+    // post.add_text("I ate a salad for lunch today");
+    // assert_eq!("", post.content());
+
+    // post.request_review();
+    // assert_eq!("", post.content());
+
+    // post.approve();
+    // assert_eq!("I ate a salad for lunch today", post.content());
+
     let mut post = Post::new();
 
     post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    let post = post.request_review();
 
-    post.approve();
+    let post = post.approve();
+
     assert_eq!("I ate a salad for lunch today", post.content());
 }
