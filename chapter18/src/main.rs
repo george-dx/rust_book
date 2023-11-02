bbaaa577d2653e718d5630015b89f8c41e9f562e
George Daian <georgedaianx@gmail.com>
2023-11-02 22:56:24 +0200

    Add while let and for conditional loops

--- chapter18/src/main.rs
+++ chapter18/src/main.rs
@@ -16,4 +16,20 @@ fn main() {
     } else {
         println!("Usingblue as the background color");
     }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{top}");
+    }
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{value} is at index {index}");
+    }
 }
