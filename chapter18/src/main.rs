4c1716152eec5c16dab5693653328b1de6e36744
George Daian <georgedaianx@gmail.com>
2023-11-08 18:43:35 +0200

    Add multiple patterns and matching ranges of values with ..=

--- chapter18/src/main.rs
+++ chapter18/src/main.rs
@@ -55,4 +55,31 @@ fn main() {
     }
 
     println!("at the end: y = {:?}, z = {z}", y);
+
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    for i in 1..=5 {
+        println!("{i}");
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
 }
