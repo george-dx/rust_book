96b985b8f3292aab89b0df564eb0b0c3b5785e03
George Daian <georgedaianx@gmail.com>
2023-11-09 22:48:06 +0200

    Destrucutre enum variants that hold different kinds of values

--- chapter18/src/main.rs
+++ chapter18/src/main.rs
@@ -3,6 +3,13 @@ struct Point {
     y: i32,
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     let first_part = false;
     if first_part {
@@ -99,6 +106,23 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {y}"),
         Point { x, y } => {
             println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The QUit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x dir {x}, in the y dir {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change color to red {r}, green {g}, and blue {b}")
         }
     }
 }
