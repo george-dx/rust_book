ecf9ec0e44064ea6e8f476f5d118b3a1de1f2b67
George Daian <georgedaianx@gmail.com>
2023-11-10 22:55:07 +0200

    Extra conditionals with match guard

--- chapter18/src/main.rs
+++ chapter18/src/main.rs
@@ -200,4 +200,31 @@ fn main() {
             println!("Some numbers: {first}, {last}");
         }
     }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
 }
