2c639d8989272516d6b06d7cb3904d93851003aa
George Daian <georgedaianx@gmail.com>
2023-11-10 22:48:57 +0200

    Ignore values using _ in a pattern

--- chapter18/src/main.rs
+++ chapter18/src/main.rs
@@ -22,6 +22,10 @@ enum Message2 {
     ChangeColor(Color),
 }
 
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {y}");
+}
+
 fn main() {
     let first_part = false;
     if first_part {
@@ -150,4 +154,30 @@ fn main() {
     }
 
     let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    foo(3, 4);
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}");
+        }
+    }
+
+    let _x = 5;
 }
