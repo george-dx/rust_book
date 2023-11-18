cfb47ffccb4f118ac5300288e07b6dc755a9e14a
George Daian <georgedaianx@gmail.com>
2023-11-18 21:41:32 +0200

    Create safe abstraction over unsafe code

--- chapter19/src/main.rs
+++ chapter19/src/main.rs
@@ -1,4 +1,20 @@
+use std::slice;
+
 unsafe fn dangerous() {}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
 
 fn main() {
     let mut num = 5;
@@ -8,6 +24,13 @@ fn main() {
 
     // let address = 0x012345usize;
     // let r = address as *const i32;
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
 
     unsafe {
         println!("r1 is: {}", *r1);
